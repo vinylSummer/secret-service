602143d9b201959d2dd5d9e5a2dcc18f27066da7
vinyl_summer <vinyl6428@gmail.com>
2024-06-27 21:32:52 +0300

    added docstrings for errors.py

--- meme_service/internal/errors/errors.py
+++ meme_service/internal/errors/errors.py
@@ -1,22 +1,28 @@
 class ServiceError(Exception):
+    """ Base class for all service errors """
     pass
 
 
 class MemeServiceError(ServiceError):
+    """ Base class for all meme service errors """
     pass
 
 
 class DBServiceError(ServiceError):
+    """ Base class for all database service errors """
     pass
 
 
 class ImageServiceError(ServiceError):
+    """ Base class for all image service errors """
     pass
 
 
-class MemeNotFoundError(MemeServiceError, DBServiceError):
+class MemeNotFoundError(DBServiceError):
+    """ Raised when a meme record is not found in database service """
     pass
 
 
-class ImageNotFoundError(ImageServiceError, DBServiceError):
+class ImageNotFoundError(ImageServiceError):
+    """ Raised when a meme image is not found in image service"""
     pass
